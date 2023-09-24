d8b0a1a0fbea3cd86572b72e03002cf526218987
Kaarel <viilvere.k@gmail.com>
2023-09-24 13:10:08 +0300

    User class

--- src/main/java/ee/sda/ticketingsystem/entity/User.java
+++ src/main/java/ee/sda/ticketingsystem/entity/User.java
@@ -1,4 +1,24 @@
 package ee.sda.ticketingsystem.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+
+@Entity
+@Getter
+@Setter
+@Accessors(chain = true)
 public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
 }
