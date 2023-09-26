4ad2b6627c7da8f2f7707fdccd948266504fcc46
Kaarel <viilvere.k@gmail.com>
2023-09-26 13:22:22 +0300

    usercontrller new

--- src/main/java/ee/sda/ticketingsystem/controller/UserController.java
+++ src/main/java/ee/sda/ticketingsystem/controller/UserController.java
@@ -3,12 +3,13 @@ package ee.sda.ticketingsystem.controller;
 import ee.sda.ticketingsystem.entity.User;
 import ee.sda.ticketingsystem.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/user")
 public class UserController {
 
     UserRepository userRepository;
@@ -18,5 +19,19 @@ public class UserController {
 
         return userRepository.save(user);
     }
+
+    @GetMapping
+    public List<User> listAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Integer id){
+        return userRepository.findById(id).get();
+    }
+
+
+
+
 
 }
